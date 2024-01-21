0a10785af4ca1ce8193e4e3afe9f66e722227fb9
Arunangshu Biswas <arunangshu.biswas@microlise.com>
2024-01-21 13:44:35 +0530

    Tried to allow cors

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const middleware = jsonServer.defaults();
 
 server.use(router);
 server.use(middleware);
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-});
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
+server.options('*', cors());
 server.listen(3000, () => {
     console.log('Server has started');
 })
