c5e257ba542f1f716966771f34cec603431738d1
Arunangshu Biswas <arunangshu.biswas@microlise.com>
2024-01-21 13:07:30 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router =jsonServer.router('journal.json');
@@ -5,7 +6,7 @@ const middleware = jsonServer.defaults();
 
 server.use(router);
 server.use(middleware);
-
+server.use(cors());
 server.listen(3000, () => {
     console.log('Server has started');
 })
